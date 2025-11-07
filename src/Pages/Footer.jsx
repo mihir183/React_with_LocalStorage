9913898a8b3d87a52c5d692f104e20c6bff16a72
mihir183 <mihirvaghela1811@gmail.com>
2025-11-08 01:19:52 +0530

    React with Local Storage

---
+++ src/Pages/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+        <div className="footer w-100 bg-dark text-light text-center p-1">
+            <p>developed by <a href="https://github.com/mihir183" target='_blank' className='text-light text-decoration-none'>@mihir183</a></p>        
+        </div> 
+    </>
+  )
+}
+
+export default Footer
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import { useForm } from 'react-hook-form'
+import { set, useForm } from 'react-hook-form'
 import '../assets/css/home.css'
+import { v4 as uuidv4 } from 'uuid';
+import Footer from './Footer';
+// import SweetAlert2 from 'react-sweetalert2';
 
 const Home = () => {
 
@@ -11,8 +14,11 @@ const Home = () => {
     const PostList = JSON.parse(localStorage.getItem("Post")) || []
     setPost(PostList)
   },[])
-
+  
   function addPost(data){
+    const id = uuidv4();
+    data.id = id
+    console.log(data)
       if(post.length === 0){
         const newData = [data]
         setPost(newData)
@@ -26,66 +32,76 @@ const Home = () => {
       }
 
   }
+
+  function trashPost(id){
+    if(confirm("Do You Delete This Post...?")){
+      const newData = post.filter((ele)=>{
+        return ele.id !== id
+      })
+      setPost(newData)
+      localStorage.setItem("Post",JSON.stringify(newData))
+    }
+
+  }
   return (
     <>
-    <section className="home">
-      <div className='pt-5'>
-        <div className='col-lg-8 m-auto mb-5 bg-light rounded-2'>
-          <button className='w-100 btn fs-4 text-capitalize' data-bs-toggle="modal" data-bs-target="#staticBackdrop">add new post </button>
-        </div>
+    {/* Background */}
+    <section className="home"></section>
+    {/* Background */}
 
-        <div className="container">
-          <div className="row">
-            {
-              post.map(ele =>
-                <div className="col-lg-4">              
-                  <div className="card p-" style={{backgroundImage: `url(${ele.profile})`, height:'500px', backgroundSize: "cover" , backgroundPosition:'center'}}>
-                    <button className='btn bg-danger btn-close position-absolute' style={{right:'10px',top:"10px"}}></button>
-                    <div className="row w-100 p-5">
-                      <div className="col text-center justify-content-end rounded-5">
-                        <h1 className='text-light '>{ele.title}</h1>
-                      </div>
-                    </div>
-                    <div className="row w-100 h-50 ">
-                      <div className="col bg-light h-50 text-center align-content-center rounded-5">
-                        <p>{ele.desc}</p>
-                      </div>
-                    </div>
+    <div className='pt-5'>
+      <div className='col-lg-8 m-auto mb-5 bg-light rounded-2'>
+        <button className='w-100 btn fs-4 text-capitalize' data-bs-toggle="modal" data-bs-target="#staticBackdrop">add new post </button>
+      </div>
+
+      <div className="container">
+        <div className="row g-4">
+          {
+            post.map(ele =>
+              <div className="col col-sm-5 col-md-4 col-lg-3 mb-3">              
+                <div className="card" style={{backgroundImage: `url(${ele.profile})`, height:'500px', backgroundSize: "cover" , backgroundPosition:'center'}}>
+                  <button className='btn bg-danger btn-close position-absolute' onClick={()=>{trashPost(ele.id)}} style={{right:'10px',top:"10px"}}></button>
+                  <div className='position-absolute bottom-0 px-3 text-center w-100'>
+                    <h2 className='text-center w-100 text-light bg-gradient d-block text-capitalize'>{ele.title}</h2>
+                    <p className='text-light bg-gradient text-capitalize'>{ele.desc}</p>
                   </div>
                 </div>
-              )
-            }
-          </div>
+              </div>
+            )
+          }
         </div>
+      </div>
 
 
-        {/* Modal */}
-        <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-          <div className="modal-dialog">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h1 className="modal-title fs-5 text-capitalize" id="staticBackdropLabel">post form</h1>
-                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div className="modal-body">
-                <form onSubmit={handleSubmit(addPost)}>
-                  <label htmlFor="image" className='form-label text-capitalize'>post image</label>
-                  <input type="url" {...register("profile")} className='form-control mb-2' placeholder='Enter Post Image/URl' id='image' />
-                  <label htmlFor="title" className='form-label text-capitalize'>post title</label>
-                  <input type="text" {...register("title")} className='form-control mb-2' placeholder='Enter Post Title' id='title' />
-                  <label htmlFor="desc" className='form-label text-capitalize'>post description</label>
-                  <textarea type="text" {...register("desc")} className='form-control mb-2' placeholder='Enter Post desc' id='desc' />
-                  <div className="text-end">
-                    <button type="submit" className="btn btn-primary text-capitalize">add</button>
-                  </div>
-                </form>
-              </div>
+      {/* Modal */}
+      <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5 text-capitalize" id="staticBackdropLabel">post form</h1>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+              <form onSubmit={handleSubmit(addPost)}>
+                <label htmlFor="image" className='form-label text-capitalize'>post image</label>
+                <input type="url" {...register("profile")} className='form-control mb-2' placeholder='Enter Post Image/URl' id='image' />
+                <label htmlFor="title" className='form-label text-capitalize'>post title</label>
+                <input type="text" {...register("title")} className='form-control mb-2' placeholder='Enter Post Title' id='title' />
+                <label htmlFor="desc" className='form-label text-capitalize'>post description</label>
+                <textarea type="text" {...register("desc")} className='form-control mb-2' placeholder='Enter Post desc' id='desc' />
+                <div className="text-end">
+                  <button type="submit" className="btn btn-primary text-capitalize" data-bs-dismiss="modal">add</button>
+                </div>
+              </form>
             </div>
           </div>
         </div>
       </div>
-    </section>
+      {/* End Model */}
+    </div>
+    <Footer/>
     </>
+
   )
 }
 
